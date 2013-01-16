ff47791b1b61c3d3a9beb15809b172a6c694df0d
Sergey Yudin <syudin@mirantis.com>
2013-01-16 10:20:18 +0000

    new roles in chef

---
+++ chef/roles/cinder-scheduler.rb
@@ -0,0 +1,8 @@
+name "cinder-scheduler"
+description "Cinder scheduler Role"
+run_list(
+         "recipe[cinder::scheduler]"
+)
+default_attributes()
+override_attributes()
+
---
+++ chef/roles/cinder-volume.rb
@@ -0,0 +1,8 @@
+name "cinder-volume"
+description "Cinder volume Role"
+run_list(
+         "recipe[cinder::volume]"
+)
+default_attributes()
+override_attributes()
+
